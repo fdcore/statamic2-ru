bd8e617887446f6c17435b4e89004f1f4db45dcc
Дмитрий Няшкин <admin@fdcore.com>
2016-04-02 03:53:20 +0300

    first commit
    in progress

---
+++ ru/fieldtypes/date.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'allow_blank' => 'Allow blank values?',
+    'allow_blank_instruct' => 'If enabled, you will be able to clear the date.',
+
+    'allow_time' => 'Allow times?',
+    'allow_time_instruct' => 'If enabled, you will be able to select a time.',
+
+];
